e82f4b863e239bf792fe4d6aef1a3e2847b43c5f
LaTuyaMuchoMas <113980904+LaTuyaMas@users.noreply.github.com>
2023-01-31 14:23:40 +0100

    componentes en ello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeriesListComponent } from './components/series-list/series-list.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/series-list/series-list.component.ts
+++ src/app/components/series-list/series-list.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Serie} from "../../common/serie";
+import {SerieService} from "../../services/serie.service";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-series-list',
   templateUrl: './series-list.component.html',
   styleUrls: ['./series-list.component.css']
 })
-export class SeriesListComponent {
+export class SeriesListComponent implements OnInit{
 
+  series: Serie[] = [];
+
+  formMovie: FormGroup = this.formBuilder.group({
+    _id: [''],
+    __v: [0],
+    images: [''],
+    title: [''],
+    categories: [''],
+    episodes: [0],
+    year: [0],
+    plot: [''],
+    user_score: this.formBuilder.group({
+      email: [''],
+      votes: [0]
+    })
+
+  });
+
+  mynewCategorie = new FormGroup({
+    newCategorie: new FormControl('')
+  });
+
+  genres: string[] = [];
+
+  editar = false;
+
+  constructor(private serieService: SerieService, private formBuilder: FormBuilder) {}
+
+  // GETTERS
+
+  // GETTERS
+
+  ngOnInit() {
+    this.listSeries();
+  }
+
+  listSeries(): void {
+    this.serieService.getSerieList().subscribe(
+      (data: any) => {
+        this.series = data;
+      }
+    );
+  }
+
+  puntuacionMedia2(serie: Serie): number{
+    let total = 0;
+    serie.user_score.forEach((user) => {
+      total += user.score;
+    });
+    let media = total / serie.user_score.length;
+    return Math.round(media * 10) / 10;
+  }
 }
