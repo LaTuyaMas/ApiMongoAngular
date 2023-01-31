34ac711a4d53278624a13d96af61814f9a68d663
LaTuyaMuchoMas <113980904+LaTuyaMas@users.noreply.github.com>
2023-01-31 13:57:44 +0100

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeriesListComponent } from './components/series-list/series-list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeriesListComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
